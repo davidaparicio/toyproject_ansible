75111272fea4a464655e3de5e58acb2b6460d64f
David Aparicio <david.aparicio@ovhcloud.com>
2024-01-24 14:14:34 +0100

    📚 docs: variadic functions

--- print.py
+++ print.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import sys
 
 def printVars(*args, **kwargs):
@@ -11,3 +12,6 @@ if __name__ == "__main__":
     # args[0] = current file
     # args[1:] = function args : (*unpacked)
     printVars(*args[1:])
+
+# https://www.executeprogram.com/courses/python-for-programmers/lessons/variadic-functions
+# https://book.pythontips.com/en/latest/args_and_kwargs.html
